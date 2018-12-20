f2d3b3b62b3cb0d481b3b74bddf9f1f769d8edc3
AnthonyKNorman <anthony.k.norman@gmail.com>
2018-12-20 14:59:22 +0000

    No longer any need to change any code for config

--- hyleton313.py
+++ hyleton313.py
@@ -5,19 +5,29 @@ light:
     name: "SOCKET1"
     state_topic: "home/socket1/status"
     command_topic: "home/socket1/set"
+    
+ From first start up the device will present an access point named something like ESP123XY
+ Connect your computer to the access point using password 'uPy12345'
+ Open a browser and browse to http://192.168.4.1 
+ You will see a web form
+ Set the name or IP address of your MQTT server
+ Give your device a unique nameEnter the SSID and Password for your wifi network
+ Sumbit
+ 
+ That's it!
 
 """
 from umqtt.simple import MQTTClient
 from machine import Pin, reset
 import utime, ujson, os, wificonf
 
-#### set these to match your requirements ####
-state_topic = b"home/socket1/status"
-command_topic = b"home/socket1/set"
-server="192.168.1.99"                      # your MQTT server
-name = 'ESPSocket1'                        # unique name for the socket
+# set topic basestring
+# e.g. set to b'home' gives topics of
+# b'home/{socket_name}/status' and b'home/{socket_name}/status'
+topic_base = b'home'
 
 #### DO NOT CHANGE ANYTHING AFTER HERE ####
+conf_file = "creds.txt"
 
 def send_status():
     """ send the on / off to home assistant """
@@ -54,22 +64,43 @@ def sub_cb(topic, msg):
 # pin 2 blue led
 # pin 4 on-board led
 # pin 15 relay
-button      = Pin(13, Pin.IN, Pin.PULL_UP)       # button on outside of plug
+button      = Pin(13, Pin.IN, Pin.PULL_UP)      # button on outside of plug
 red_led     = Pin(0, Pin.OUT)                   # red led on front
 blue_led    = Pin(2, Pin.OUT)                   # bue led on front
 onboard_led = Pin(4, Pin.OUT)                   # blue led on ESP8266 board(not visible)
 relay       = Pin(15, Pin.OUT)                  # relay
-state = "OFF"                                    # socket status
-relay.off()
+
+state = "OFF"                                   # socket status
+relay.off()                                     # set state as off
 red_led.on()
 blue_led.off()
 
-wificonf.wificonf(name)
+w = wificonf.wificonf(cFile = conf_file)        # initialises the config file if it doesn't exist
+while not w.wifi():                             # start the wifi
+    w.get_params()                              # enable the web interface if the wifi doesn't connect
+    
+print (w.c)
 
-# connec to mqtt
-c = MQTTClient(name, server)
-c.set_callback(sub_cb)                          # set the callback function
-c.connect()                                     # get connected
+state_topic   = topic_base + b'/'+ w.c['name'].encode() + b'/status'
+command_topic = topic_base + b'/'+ w.c['name'].encode() + b'/set'
+print (state_topic, command_topic)
+
+
+mqtt = 0
+while not mqtt:
+    # connect to mqtt
+    print('mqtt name: {} mqtt address {}'.format(w.c['name'], w.c['mqtt']))
+    c = MQTTClient(w.c['name'], w.c['mqtt'])
+    c.set_callback(sub_cb)                      # set the callback function
+    try:
+        c.connect()                             # get connected
+        mqtt = 1
+    except Exception as e:
+        print('MQTT connection failed', e)
+        w.get_params()                          # enable the web interface if the wifi doesn't connect
+        reset()
+        
+        
 c.subscribe(command_topic)                      # subscribe to command topic    
 
 send_status()                                   # update home assistant with light on / off, rgb and brightness
@@ -80,7 +111,7 @@ while True:
     now = utime.ticks_ms()
     while button.value() == 0:
         if utime.ticks_diff(utime.ticks_ms(), now) > 5000:
-            os.remove('creds.txt')
+            os.remove(conf_file)
             reset()
     utime.sleep(.5)
 c.disconnect()                                  # if we come out of the loop for any reason then disconnect
