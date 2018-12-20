df3557840c0f68e21e690c71c4e72fb9829b5261
AnthonyKNorman <anthony.k.norman@gmail.com>
2018-12-20 14:58:31 +0000

    Added the ability to set the timer period

--- timer.py
+++ timer.py
@@ -6,17 +6,16 @@ micropython.alloc_emergency_exception_buf(100)
  
 class led_toggle(object):
     # set up a timer interrupt to toggle a led
-    def __init__(self, led):
+    def __init__(self, led, msecs=500):
         self.led = led
         # this is used to remember the led state
         self._value = False
         # set up the timer
         self.timer = Timer(0)
         # start the timer
-        # 2 second period
         # PERIODIC mode means that it repeats
         # uses the cb function when timer triggers
-        self.timer.init(period=500, mode=Timer.PERIODIC, callback=self.cb)
+        self.timer.init(period=msecs, mode=Timer.PERIODIC, callback=self.cb)
          
     def cb(self, timer):
         # timer callback function
